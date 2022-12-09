2bc714088803f9383b8715d4c64cb4c1159c3c55
Shuvro Das <70909095+Shuvro-d@users.noreply.github.com>
2022-12-09 10:57:16 +0600

    Update and rename detect7.py to confusion_matrix_for_testdata_v7.py

--- confusion_matrix_for_testdata_v7.py
+++ confusion_matrix_for_testdata_v7.py
@@ -21,8 +21,8 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized,
 
 def detect(save_img=False):
     source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
-    source1=source+"/stone"
-    source2=source+'/normal'
+    source1=source+"/positive"
+    source2=source+'/negative'
 ################################################### First Part ##############################################################
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
